12b5fdee8cd3541de0676a9fe53f0901735a74f8
Matthew DeCoste <mcdecoste@gmail.com>
2015-02-06 20:18:46 -0800

    Initial Commit

---
+++ BruinLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BruinLoader
+//
+//  Created by Matthew DeCoste on 2/6/15.
+//  Copyright (c) 2015 Matthew DeCoste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
